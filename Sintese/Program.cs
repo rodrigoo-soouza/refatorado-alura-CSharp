7eb50b754b6e28bfb4a82a5a24386c83102fb85d
rodrigoo-soouza <digosouzafonseca@gmail.com>
2023-06-25 21:55:02 -0300

    criando

---
+++ Sintese/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {   
+        Console.Write("Digite a idade de João: ");
+        int idade = Convert.ToInt32(Console.ReadLine());
+        
+        Console.Write("Digite True para acompanhado e False para desacompanhado: ");
+        bool acompanhado = Convert.ToBoolean(Console.ReadLine());
+        
+
+        if (idade >= 18)
+
+            Console.WriteLine("João pode entrar");
+
+        else if (idade <= 18 && acompanhado == true)
+
+            Console.WriteLine("João é menor de idade mas está acompanhado, pode entrar.");
+
+        else
+
+            Console.WriteLine("João é menor de idade e está desacompanhado não pode entrar");
+        Console.ReadKey();
+    }
+    
+}  
